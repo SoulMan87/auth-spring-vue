d37cc3ae29e564b7fe0789bd860b7b213b6383f8
jonathan <jonathanhinestroza87@gmail.com>
2023-11-03 18:14:47 -0500

    Step (#3) - Repository ready

---
+++ src/main/java/com/soulrebel/auth/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.soulrebel.auth.repository;
+
+import com.soulrebel.auth.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
